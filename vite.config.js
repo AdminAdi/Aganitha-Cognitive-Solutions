a9325ffc0bbab66e93fdbd7a8f62abd7f297520c
Aditya <itsmeaditya3254@gmail.com>
2025-09-17 13:48:02 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
